58e4aacd14428af19c5c95d1b9e71615c8343778
rosieomusic <rosieomusic@gmail.com>
2025-04-24 13:05:15 -0400

    added new resume draft

--- .history/src/sections/Hero/Hero_20250328201619.jsx
+++ .history/src/sections/Hero/Hero_20250328201619.jsx
@@ -6,7 +6,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedInLight from '../../assets/linkedin-light.svg';
 import linkedInDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/Rose_OMalley_Resume2025.pdf';
+import CV from '../../assets/Rose_OMalley_Resume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 import { Link } from 'react-router-dom';
 
